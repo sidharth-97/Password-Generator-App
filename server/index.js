1ee472aa3ee42211276d06a24942eff0b0110c11
Sidharth PT <sidharthanand73@gmail.com>
2024-01-15 19:54:58 +0530

    fix:handle cors error

--- server/index.js
+++ server/index.js
@@ -3,10 +3,13 @@ const express = require("express")
 const mongoose = require('mongoose')
 const router = require('./routes/route')
 const session = require('express-session');
+const cors=require("cors")
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors({ origin: process.env.WEBSITE, credentials: true }));
+app.options("*", cors());
 app.use("/api",router)
 app.use(
     session({
@@ -15,7 +18,7 @@ app.use(
         saveUninitialized: true,
     })
 );
-  
+
 try {
     mongoose.connect(process.env.MONGO_URL,console.log("Database connected"))
 } catch (error) {
