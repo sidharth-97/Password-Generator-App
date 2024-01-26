ae32395f809e0b2c29cf61b8077c5ca85a07a198
Sidharth PT <sidharthanand73@gmail.com>
2024-01-26 22:53:38 +0530

    Used local storage with context api

--- client/src/components/AuthContext.jsx
+++ client/src/components/AuthContext.jsx
@@ -1,15 +1,27 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 const AuthContext = createContext()
 
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null)
+
+    useEffect(() => {
+        const userData = localStorage.getItem('user')
+        if (userData) {
+            setUser(JSON.parse(userData))
+        }
+    }, [])
+
     const loginC = (userData) => {
         setUser(userData)
+        localStorage.setItem('user', JSON.stringify(userData)) 
     }
+
     const logoutC = () => {
         setUser(null)
+        localStorage.removeItem('user') 
     }
+
     return (
         <AuthContext.Provider value={{ user, loginC, logoutC }}>
             {children}
